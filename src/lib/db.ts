a6e91d16c3f3723fa15059aa017e4898f8678ea7
Felipozzi-OS <marcelofelipozzi@gmail.com>
2026-08-11 17:55:08 -0300

    fix: use correct export name from @prisma/adapter-libsql (PrismaLibSql)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
@@ -9,7 +9,7 @@ const libsql = createClient({
   authToken: process.env.TURSO_AUTH_TOKEN as string,
 })
 
-const adapter = new PrismaLibSQL(libsql)
+const adapter = new PrismaLibSql(libsql)
 
 export const db =
   globalForPrisma.prisma ??
